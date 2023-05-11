f7b247da632ed9244feccefed35cf11f4b4182c8
Jeferson <jefersonvm2@hotmail.com>
2023-05-11 20:26:04 -0300

    Barra de status

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View, StyleSheet, ScrollView, Text } from "react-native";
 import {useNavigation} from "@react-navigation/native";
 import LifeStatus from "../../Components/Common/LifeStatus";
+import StatusBar from "../../Components/Home/StatusBar";
 
 export default function Home(){
   const navigation = useNavigation();
@@ -16,6 +17,7 @@ export default function Home(){
             ❤️ 20 dias - ✔️ 80 checks
           </Text>
           <LifeStatus/>
+          <StatusBar/>
         </View>
         <Text 
           style={styles.explanationText}
