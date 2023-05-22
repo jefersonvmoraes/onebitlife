2f14b9588441c42adc52e5cf5d88ecae55dc47d1
Jeferson <jefersonvm2@hotmail.com>
2023-05-21 22:17:26 -0300

    Renderizacao condicional de pagina e tempo de vida dinamico

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -5,8 +5,9 @@ import LifeStatus from "../../Components/Common/LifeStatus";
 import StatusBar from "../../Components/Home/StatusBar";
 import CreateHabit from "../../Components/Home/CreateHabit";
 import EditHabit from "../../Components/Home/EditHabit";
+import ChangeNavigationService from "../../Services/ChangeNavigationService";
 
-export default function Home(){
+export default function Home({route}){
   const navigation = useNavigation();
 
   const [mindHabit, setMindHabit] = useState();
@@ -14,16 +15,30 @@ export default function Home(){
   const [bodyHabit, setBodyHabit] = useState();
   const [funHabit, setFunHabit] = useState();
 
+  const [robotDaysLife, setRobotDaysLife] = useState();
+  const today = new Date();
+
   function handleNavExplanation (){
     navigation.navigate("AppExplanation")
   }
+
+  useEffect(()=>{
+    ChangeNavigationService.checkShowHome(1)
+      .then((showHome)=>{
+        const formDate = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`;
+        const checkDays = 
+          new Date(formDate) - new Date(showHome.appStartData) + 1;
+        setRobotDaysLife(checkDays.toString().padStart(2, "0"));
+      })
+      .catch((err)=> console.log(err));
+  },[route.params]);
 
   return(
     <View style={styles.container}>
       <ScrollView>
         <View style={{alignItems: "center"}}>
           <Text style={styles.dailyChecks}>
-            ❤️ 20 dias - ✔️ 80 checks
+            ❤️ {robotDaysLife} {robotDaysLife === "01" ? "dia" : "dias"} - ✔️ 80 checks
           </Text>
           <LifeStatus/>
           <StatusBar/>
